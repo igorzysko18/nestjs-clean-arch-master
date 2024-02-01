bbf22394e7065d2e91f336d33beee0fbc47b1682
Igor Zysko <igorzysko18@hotmail.com>
2024-01-31 22:04:28 -0300

    Finalizado config dotenv

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -4,10 +4,12 @@ import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class EnvConfigService implements EnvConfig {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private configService: ConfigService) {}
+
   getAppPort(): number {
     return Number(this.configService.get<number>('PORT'))
   }
+
   getNodeEnv(): string {
     return this.configService.get<string>('NODE_ENV')
   }
