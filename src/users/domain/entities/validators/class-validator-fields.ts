5b355eddaec69b51b97d11e85ff6243f8a2aed8f
Igor Zysko <igorzysko18@hotmail.com>
2024-02-05 21:26:01 -0300

    unit tests

--- src/users/domain/entities/validators/class-validator-fields.ts
+++ src/users/domain/entities/validators/class-validator-fields.ts
@@ -1,3 +1,4 @@
+import { validateSync } from 'class-validator'
 import {
   FieldsErrors,
   ValidatorFieldsInterface,
@@ -6,9 +7,21 @@ import {
 export abstract class ClassValidatorFields<PropsValidated>
   implements ValidatorFieldsInterface<PropsValidated>
 {
-  errors: FieldsErrors
-  validateData: PropsValidated
+  errors: FieldsErrors = null
+  validateData: PropsValidated = null
+
   validate(data: any): boolean {
-    throw new Error('Method not implemented.')
+    const errors = validateSync(data)
+    if (errors.length) {
+      this.errors = {}
+      for (const error of errors) {
+        const field = error.property
+        this.errors[field] = Object.values(error.constraints)
+      }
+    } else {
+      this.validateData = data
+    }
+
+    return !errors.length
   }
 }
