06780c426e6a49a91386e6f1797fc881134a0f94
Igor Zysko <igorzysko18@hotmail.com>
2024-02-13 18:30:40 -0300

    Add searchable repository

---
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -0,0 +1,63 @@
+import { Entity } from '../entities/entity'
+import { RepositoryInterface } from './repository-contracts'
+
+export type SortDirection = 'asc' | 'desc'
+
+export type SearchProps<Filter = string> = {
+  page?: number
+  perpage?: number
+  sort?: string | null
+  sortDir: SortDirection | null
+  filter?: Filter | null
+}
+
+export class SearchParams {
+  protected _page: number
+  protected _perPage = 15
+  protected _sort: string | null
+  protected _sortDir: SortDirection | null
+  protected _filter: string | null
+
+  constructor(public props: SearchProps) {
+    this._page = props.page
+    this._perPage = props.perpage
+    this._sort = props.sort
+    this._sortDir = props.sortDir
+    this._filter = props.filter
+  }
+
+  get page() {
+    return this._page
+  }
+
+  private set page(value: number) {}
+
+  get perPage() {
+    return this._perPage
+  }
+  private set perPage(value: number) {}
+
+  get sort() {
+    return this._sort
+  }
+
+  private set sort(value: string | null) {}
+
+  get sortDir() {
+    return this._sortDir
+  }
+
+  private set sortDir(value: string | null) {}
+
+  get filter() {
+    return this.filter
+  }
+  private set filter(value: string | null) {}
+}
+export interface SearchableRepositoryInterface<
+  E extends Entity,
+  SearchInput,
+  SearchOutput,
+> extends RepositoryInterface<E> {
+  search(props: SearchParams): Promise<SearchOutput>
+}
