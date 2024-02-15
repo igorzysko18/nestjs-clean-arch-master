3853192602826ac96f15f133d4393c706da35417
Igor Zysko <igorzysko18@hotmail.com>
2024-02-15 20:07:21 -0300

    testes unit pt 2

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -130,8 +130,9 @@ export class SearchResult<E extends Entity, Filter = string> {
 }
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchInput,
-  SearchOutput,
+  Filter = string,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult<Entity, Filter>,
 > extends RepositoryInterface<E> {
-  search(props: SearchParams): Promise<SearchOutput>
+  search(props: SearchInput): Promise<SearchOutput>
 }
