29b579b163a3bade64abdfbe17216243c39cf7fb
Igor Zysko <igorzysko18@hotmail.com>
2024-02-17 19:13:25 -0300

    ApllySort method unit test

---
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -0,0 +1,105 @@
+import { Entity } from '@/shared/domain/entities/entity'
+import { InMemorySearchableRepository } from '../../in-memory-searchable.repository'
+
+type StubEntityProps = {
+  name: string
+  price: number
+}
+
+class StubEntity extends Entity<StubEntityProps> {}
+
+class StubInMemorySearchableRepository extends InMemorySearchableRepository<StubEntity> {
+  sortableFields: string[] = ['name']
+
+  protected async applyFilter(
+    items: StubEntity[],
+    filter: string | null,
+  ): Promise<StubEntity[]> {
+    if (!filter) {
+      return items
+    }
+
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLocaleLowerCase())
+    })
+  }
+}
+
+describe('InMemorySearchableRepository unit tests', () => {
+  let sut: StubInMemorySearchableRepository
+
+  beforeEach(() => {
+    sut = new StubInMemorySearchableRepository()
+  })
+  describe('applyFilter method', () => {
+    it('should no filter items when filter param is null', async () => {
+      const items = [new StubEntity({ name: 'name value', price: 50 })]
+      const SpyFilterMethod = jest.spyOn(items, 'filter')
+      const itemsFiltered = await sut['applyFilter'](items, null)
+      expect(itemsFiltered).toStrictEqual(items)
+      expect(SpyFilterMethod).not.toHaveBeenCalled()
+    })
+
+    it('should filter using a filter param', async () => {
+      const items = [
+        new StubEntity({ name: 'test', price: 50 }),
+        new StubEntity({ name: 'TEST', price: 50 }),
+        new StubEntity({ name: 'fake', price: 50 }),
+      ]
+      const SpyFilterMethod = jest.spyOn(items, 'filter')
+      let itemsFiltered = await sut['applyFilter'](items, 'TEST')
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]])
+      expect(SpyFilterMethod).toHaveBeenCalledTimes(1)
+
+      itemsFiltered = await sut['applyFilter'](items, 'test')
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]])
+      expect(SpyFilterMethod).toHaveBeenCalledTimes(2)
+
+      itemsFiltered = await sut['applyFilter'](items, 'no-filter')
+      expect(itemsFiltered).toHaveLength(0)
+      expect(SpyFilterMethod).toHaveBeenCalledTimes(3)
+    })
+  })
+  describe('applySort method', () => {
+    it('should no filter items when filter param is null', async () => {
+      const items = [new StubEntity({ name: 'name value', price: 50 })]
+      const SpyFilterMethod = jest.spyOn(items, 'filter')
+      const itemsFiltered = await sut['applyFilter'](items, null)
+      expect(itemsFiltered).toStrictEqual(items)
+      expect(SpyFilterMethod).not.toHaveBeenCalled()
+    })
+
+    it('should no sort items', async () => {
+      const items = [
+        new StubEntity({ name: 'a', price: 50 }),
+        new StubEntity({ name: 'b', price: 50 }),
+      ]
+
+      let itemsSorted = await sut['applySort'](items, null, null)
+      expect(itemsSorted).toStrictEqual(items)
+
+      itemsSorted = await sut['applySort'](items, 'price', 'asc')
+      expect(itemsSorted).toStrictEqual(items)
+    })
+
+    it('should sort items', async () => {
+      const items = [
+        new StubEntity({ name: 'b', price: 50 }),
+        new StubEntity({ name: 'a', price: 50 }),
+        new StubEntity({ name: 'c', price: 50 }),
+      ]
+
+      let itemsSorted = await sut['applySort'](items, 'name', 'asc')
+
+      expect(itemsSorted).toStrictEqual([items[1], items[0], items[2]])
+
+      itemsSorted = await sut['applySort'](items, 'name', 'desc')
+
+      expect(itemsSorted).toStrictEqual([items[2], items[0], items[1]])
+    })
+  })
+
+  describe('applyPaginate method', () => {})
+
+  describe('search method', () => {})
+})
